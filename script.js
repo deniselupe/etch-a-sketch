ee875844c0710bd230662414efc62b8eb7f4e3e6
deniselupe <dgrodriguez24@hotmail.com>
2021-08-02 15:30:48 +0000

    add: change grid-item color when hovered over

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ for (let i = 0; i < squareAreaNumber ** 2; i++) {
 	gridItem.classList.add('grid-item');
 	gridContainer.appendChild(gridItem);
 }
+
+let gridItem = Array.from(document.querySelectorAll('.grid-item'));
+
+gridItem.forEach((item) => {
+	item.addEventListener('mouseover', function () {
+		item.setAttribute('style', 'background-color: black;');
+	});
+});
