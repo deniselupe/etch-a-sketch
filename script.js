706ceba524c5c88c3b89a83662b756dc3e144992
deniselupe <dgrodriguez24@hotmail.com>
2021-08-07 17:01:55 +0000

    feat: change drawing color using color selector

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let gridParent = document.querySelector('.grid-parent');
-let resetButton = document.querySelector('.reset-button')
+let colorSelector = document.getElementById('color-selector');
 let clearGridButton = document.getElementById('reset-button');
-
+let colorChoice = 'rgb(0, 0, 0)';
 
 function createGridChildren(squareAreaNumber) {
 	for (let i = 0; i < squareAreaNumber ** 2; i++) {
@@ -13,17 +13,44 @@ function createGridChildren(squareAreaNumber) {
 	gridParent.style.gridTemplate = `repeat(${squareAreaNumber}, 1fr) / repeat(${squareAreaNumber}, 1fr)`;
 	let gridChild = Array.from(document.querySelectorAll('.grid-child'));
 	
-	function colorGrid(event) {
+	function coloringRule(event) {
 		event.preventDefault();
-		if (event.buttons ===1) event.target.style.backgroundColor = 'black';
+		if (event.buttons === 1) event.target.style.backgroundColor = colorChoice;
 	}
 	
 	gridChild.forEach((item) => {
-		item.addEventListener('mousedown', colorGrid);
-		item.addEventListener('mousemove', colorGrid);
+		item.addEventListener('mousedown', coloringRule);
+		item.addEventListener('mousemove', coloringRule);
 	});
 }
 
+//Hex to RGB Formula
+function hexToRGB(hex) {
+	let r = 0;
+	let g = 0;
+	let b = 0;
+
+	// 3 digits
+	if (hex.length == 4) {
+		r = "0x" + hex[1] + hex[1];
+		g = "0x" + hex[2] + hex[2];
+		b = "0x" + hex[3] + hex[3];
+	// 6 digits
+	} else if (hex.length == 7) {
+		r = "0x" + hex[1] + hex[2];
+		g = "0x" + hex[3] + hex[4];
+		b = "0x" + hex[5] + hex[6];
+	}
+
+	return `rgb(${+r}, ${+g}, ${+b})`;
+}
+
+//Color Input Button
+colorSelector.addEventListener('change', (event) => {
+	colorChoice = hexToRGB(event.target.value);
+});
+
+//Reset Grid Button
 clearGridButton.addEventListener('click', () => {
 	let gridNum = prompt('How many squares per side for the next grid?', '(Number must be less than or equal to 100)');
 	if (gridNum > 100) {
@@ -40,4 +67,3 @@ clearGridButton.addEventListener('click', () => {
 });
 
 createGridChildren(16);
-
